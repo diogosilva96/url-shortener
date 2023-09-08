a5094ad27f5cb04c52987d3dbd1aa44f97fb2bda
Diogo Silva <diogo_silva_96@hotmail.com>
2023-09-08 21:26:06 +0100

    Add ValidationPipelineBehaviour

--- src/Url.Shortener.Api/Domain/ServiceCollectionExtensions.cs
+++ src/Url.Shortener.Api/Domain/ServiceCollectionExtensions.cs
@@ -10,7 +10,11 @@ internal static class ServiceCollectionExtensions
     public static IServiceCollection AddDomainServices(this IServiceCollection serviceCollection, Action<UrlShortenerOptions> configureUrlShortenerOptions)
     {
         return serviceCollection.AddCarter()
-                                .AddMediatR(config => config.Lifetime = ServiceLifetime.Transient)
+                                .AddMediatR(config =>
+                                {
+                                    config.Lifetime = ServiceLifetime.Transient;
+                                    config.AddOpenBehavior(typeof(ValidationPipelineBehaviour<,>));
+                                })
                                 .AddSingleton<ISystemClock, SystemClock>()
                                 .AddValidatorsFromAssembly(typeof(Program).Assembly)
                                 .AddCreateUrlServices(configureUrlShortenerOptions);
---
+++ src/Url.Shortener.Api/Domain/ValidationPipelineBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+
+namespace Url.Shortener.Api.Domain;
+
+public class ValidationPipelineBehaviour<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationPipelineBehaviour(IEnumerable<IValidator<TRequest>> validators) => _validators = validators;
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var validationFailures = new List<ValidationFailure>();
+
+        foreach (var validator in _validators)
+        {
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
+            if (validationResult.IsValid) continue;
+            validationFailures.AddRange(validationResult.Errors);
+        }
+
+        if (!validationFailures.Any())
+        {
+            throw new ValidationException(validationFailures);
+        }
+
+        return await next();
+    }
+}

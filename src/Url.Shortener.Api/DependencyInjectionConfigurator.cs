812d4ddaf9486b56348aab5566f13de8a771477d
Diogo Silva <diogo_silva_96@hotmail.com>
2023-09-08 19:46:00 +0100

    Add DependencyInjectionConfigurator

---
+++ src/Url.Shortener.Api/DependencyInjectionConfigurator.cs
@@ -0,0 +1,17 @@
+﻿namespace Url.Shortener.Api;
+
+public static class DependencyInjectionConfigurator
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection serviceCollection,
+        IConfiguration configuration,
+        IHostEnvironment hostEnvironment)
+    {
+        ArgumentNullException.ThrowIfNull(serviceCollection);
+        ArgumentNullException.ThrowIfNull(configuration);
+        ArgumentNullException.ThrowIfNull(hostEnvironment);
+
+        return serviceCollection.AddControllers().Services
+                                .AddEndpointsApiExplorer()
+                                .AddSwaggerGen();
+    }
+}

31c07e192a23154b09f9e912a0ed4c55425df351
Diogo Silva <diogo_silva_96@hotmail.com>
2023-09-08 18:59:35 +0100

    Add Data.Migrator implementation

---
+++ tools/Url.Shortener.Data.Migrator/DataMigrator.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Url.Shortener.Data.Migrator;
+
+public class DataMigrator : BackgroundService
+{
+    private readonly IHostApplicationLifetime _applicationLifetime;
+    private readonly ILogger<DataMigrator> _logger;
+    private readonly IServiceProvider _serviceProvider;
+
+    public DataMigrator(IHostApplicationLifetime applicationLifetime,
+        IServiceProvider serviceProvider,
+        ILogger<DataMigrator> logger)
+    {
+        _applicationLifetime = applicationLifetime ?? throw new ArgumentNullException(nameof(applicationLifetime));
+        _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+    
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using var scope = _serviceProvider.CreateScope();
+        await using var dbContext = scope.ServiceProvider.GetRequiredService<UrlShortenerDbContext>();
+
+        await ApplyDatabaseMigrations(stoppingToken, dbContext);
+
+        await StopAsync(stoppingToken);
+    }
+
+    private async Task ApplyDatabaseMigrations(CancellationToken stoppingToken,
+        DbContext dbContext)
+    {
+        _logger.LogInformation("Starting data migration...");
+
+        await dbContext.Database.MigrateAsync(stoppingToken);
+
+        _logger.LogInformation("Data migration completed.");
+    }
+
+    public override async Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Data Migrator started...");
+        await base.StartAsync(cancellationToken);
+    }
+
+    public override async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await base.StopAsync(cancellationToken);
+
+        _applicationLifetime.StopApplication();
+    }
+}

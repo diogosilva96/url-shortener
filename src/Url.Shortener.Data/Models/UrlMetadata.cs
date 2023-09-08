30aa443e8e49df8f0735f4da85a78dfc879f0920
Diogo Silva <diogo_silva_96@hotmail.com>
2023-09-08 18:25:59 +0100

    Add UrlShortenerDbContext, UrlMetadata & UrlMetadataConfiguration

---
+++ src/Url.Shortener.Data/Models/UrlMetadata.cs
@@ -0,0 +1,12 @@
+﻿namespace Url.Shortener.Data.Models;
+
+public class UrlMetadata
+{
+    public Guid Id { get; init; }
+
+    public string ShortUrl { get; set; } = string.Empty;
+    
+    public string FullUrl { get; set; } = string.Empty;
+    
+    public DateTimeOffset CreatedAtUtc { get; set; }
+}
---
+++ src/Url.Shortener.Data/UrlShortenerDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Url.Shortener.Data.Models;
+
+namespace Url.Shortener.Data;
+
+public class UrlShortenerDbContext : DbContext
+{
+    public UrlShortenerDbContext(DbContextOptions<UrlShortenerDbContext> dbContextOptions) : base(dbContextOptions) { }
+
+    public DbSet<UrlMetadata> UrlMetadata => Set<UrlMetadata>();
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(UrlShortenerDbContext).Assembly);
+        
+        base.OnModelCreating(modelBuilder);
+    }
+}

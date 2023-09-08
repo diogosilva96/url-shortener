5a73a1d5b50649253f0251b83a23f82d7e5aaa92
Diogo Silva <diogo_silva_96@hotmail.com>
2023-09-08 19:45:49 +0100

    Add MiddlewareConfigurator

---
+++ src/Url.Shortener.Api/MiddlewareConfigurator.cs
@@ -0,0 +1,29 @@
+﻿using Serilog;
+
+namespace Url.Shortener.Api;
+
+public static class MiddlewareConfigurator
+{
+    public static WebApplication ConfigureMiddleware(this WebApplication webApplication)
+    {
+        ConfigureSwagger(webApplication);
+
+        webApplication.UseSerilogRequestLogging()
+                      .UseHsts()
+                      .UseHttpsRedirection()
+                      .UseAuthorization();
+
+        // TODO: remove & use minimal apis
+        webApplication.MapControllers();
+
+        return webApplication;
+    }
+
+    private static void ConfigureSwagger(WebApplication webApplication)
+    {
+        if (!webApplication.Environment.IsDevelopment()) return;
+        
+        webApplication.UseSwagger();
+        webApplication.UseSwaggerUI();
+    }
+}

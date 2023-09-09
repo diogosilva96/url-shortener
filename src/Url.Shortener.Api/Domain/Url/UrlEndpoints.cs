1ea1fdf301072493c5376ad05068ac956878b382
Diogo Silva <diogo_silva_96@hotmail.com>
2023-09-09 02:44:16 +0100

    Fix UrlEndpoints

--- src/Url.Shortener.Api/Domain/Url/UrlEndpoints.cs
+++ src/Url.Shortener.Api/Domain/Url/UrlEndpoints.cs
@@ -9,7 +9,8 @@ public class UrlEndpoints : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        var group = app.MapGroup("api/urls");
+        var group = app.MapGroup("api/urls")
+                       .WithOpenApi();
 
         group.MapPost("", async (CreateUrlRequest request, IMediator mediator) =>
              {
@@ -17,6 +18,7 @@ public class UrlEndpoints : ICarterModule
                  return await mediator.Send(domainRequest);
              })
              .WithName("CreateUrl")
+             .WithDescription("Creates a short url based on the specified request.")
              .Produces<string>()
              .Produces<HttpValidationProblemDetails>(StatusCodes.Status400BadRequest)
              .Produces<ProblemHttpResult>(StatusCodes.Status500InternalServerError);

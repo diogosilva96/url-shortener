8873f681950a2b5a7e067e9471ae83b7f09b5892
Diogo Silva <diogo_silva_96@hotmail.com>
2023-09-09 01:28:35 +0100

    Add ValidationProcessor

---
+++ src/Url.Shortener.Api/Domain/ValidationProcessor.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using MediatR.Pipeline;
+
+namespace Url.Shortener.Api.Domain;
+
+internal class ValidationProcessor<TRequest> : IRequestPreProcessor<TRequest> where TRequest : notnull
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+    private readonly ILogger<ValidationProcessor<TRequest>> _logger;
+    
+    public ValidationProcessor(IEnumerable<IValidator<TRequest>> validators, ILogger<ValidationProcessor<TRequest>> logger)
+    {
+        _validators = validators;
+        _logger = logger;
+    }
+
+    public async Task Process(TRequest request, CancellationToken cancellationToken)
+    {
+        var validationFailures = new List<ValidationFailure>();
+
+        foreach (var validator in _validators)
+        {
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
+            if (validationResult.IsValid) continue;
+            validationFailures.AddRange(validationResult.Errors);
+        }
+
+        if (!validationFailures.Any()) return;
+        
+        var validationException = new ValidationException(validationFailures);
+        _logger.LogError(validationException, "Validation failed for object of type '{ObjectType}'", typeof(TRequest).FullName);
+        throw validationException;
+    }
+}

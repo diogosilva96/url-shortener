4cd70996c486c9b9bedf349cd4c07bd101b387cb
Diogo Silva <diogo_silva_96@hotmail.com>
2023-09-09 02:25:56 +0100

    Fix url shortener

--- src/Url.Shortener.Api/Domain/Url/Create/UrlShortener.cs
+++ src/Url.Shortener.Api/Domain/Url/Create/UrlShortener.cs
@@ -19,7 +19,7 @@ internal class UrlShortener : IUrlShortener
         var sb = new StringBuilder();
         for (var i = 0; i < _urlSize; i++)
         {
-            var characterIndex = Random.Shared.Next(0, _urlSize);
+            var characterIndex = Random.Shared.Next(0, _characters.Length);
             sb.Append(_characters[characterIndex]);
         }
 

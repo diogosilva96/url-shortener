9c906bf4eab65ddd8398376b4b5d0a341f61ab25
Diogo Silva <diogo_silva_96@hotmail.com>
2023-09-26 17:57:24 +0100

    Add unit tests for 'AddCreateUrlServices' DI extension method

---
+++ test/Url.Shortener.Api.UnitTests/Domain/Url/ServiceCollectionExtensionsFixture/WhenBuildingServiceProvider.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Url.Shortener.Api.Domain.Url.Create;
+using Url.Shortener.Api.Tests.Common.Domain.Url.Builder;
+using Url.Shortener.Api.UnitTests.Builder;
+using Xunit;
+
+namespace Url.Shortener.Api.UnitTests.Domain.Url.ServiceCollectionExtensionsFixture;
+
+public class WhenBuildingServiceProvider
+{
+    private readonly Action<UrlShortenerOptions> _configureUrlShortenerOptions;
+    private readonly IServiceCollection _serviceCollection;
+
+    public WhenBuildingServiceProvider()
+    {
+        var urlShortenerOptions = new UrlShortenerOptionsBuilder().Build();
+        _configureUrlShortenerOptions = options =>
+        {
+            options.Characters = urlShortenerOptions.Characters;
+            options.UrlSize = urlShortenerOptions.UrlSize;
+        };
+
+        _serviceCollection = ServiceCollectionBuilder.Build();
+    }
+
+    [Fact]
+    public void TheNoExceptionIsThrown()
+    {
+        var exception = Record.Exception(WhenBuilding);
+
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void TheAUrlShortenerCanBeRetrieved()
+    {
+        var provider = WhenBuilding();
+
+        Assert.NotNull(provider.GetService<IUrlShortener>());
+    }
+
+    [Fact]
+    public void TheAUrlShortenerOptionsCanBeRetrieved()
+    {
+        var provider = WhenBuilding();
+
+        Assert.NotNull(provider.GetService<IOptions<UrlShortenerOptions>>());
+    }
+
+    private IServiceProvider WhenBuilding() => _serviceCollection.AddCreateUrlServices(_configureUrlShortenerOptions)
+                                                                 .BuildServiceProvider();
+}

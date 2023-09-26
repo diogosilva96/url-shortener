8cdf99d67a2bec841d8deef809b0cde82ff98d09
Diogo Silva <diogo_silva_96@hotmail.com>
2023-09-26 17:28:52 +0100

    Add ServiceCollectionBuilder

---
+++ test/Url.Shortener.Api.UnitTests/Builder/ServiceCollectionBuilder.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Url.Shortener.Api.UnitTests.Builder;
+
+internal static class ServiceCollectionBuilder
+{
+    public static IServiceCollection Build() => new ServiceCollection();
+}

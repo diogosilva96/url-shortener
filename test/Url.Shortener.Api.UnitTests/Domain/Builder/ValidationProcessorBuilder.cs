6376795b494b117e38e2b47c4fe058c018ebd366
Diogo Silva <diogo_silva_96@hotmail.com>
2023-09-26 18:18:56 +0100

    Add ValidationProcessorBuilder

---
+++ test/Url.Shortener.Api.UnitTests/Domain/Builder/ValidationProcessorBuilder.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Url.Shortener.Api.Domain;
+
+namespace Url.Shortener.Api.UnitTests.Domain.Builder;
+
+internal class ValidationProcessorBuilder<T> where T : IValidatableRequest
+{
+    private ILogger<ValidationProcessor<T>> _logger;
+    private IValidator<T> _validator;
+
+    public ValidationProcessorBuilder()
+    {
+        _validator = Substitute.For<IValidator<T>>();
+        _logger = Substitute.For<ILogger<ValidationProcessor<T>>>();
+    }
+
+    public ValidationProcessor<T> Build() => new(_validator, _logger);
+
+    public ValidationProcessorBuilder<T> With(IValidator<T> validator)
+    {
+        _validator = validator;
+
+        return this;
+    }
+
+    public ValidationProcessorBuilder<T> With(ILogger<ValidationProcessor<T>> logger)
+    {
+        _logger = logger;
+
+        return this;
+    }
+}

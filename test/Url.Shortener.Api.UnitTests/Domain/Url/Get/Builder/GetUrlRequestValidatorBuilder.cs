03fbd364e8a4892464705fd1949ca065c3242a0d
Diogo Silva <diogo_silva_96@hotmail.com>
2023-09-26 18:55:26 +0100

    Add unit tests for GetUrlRequestValidator

---
+++ test/Url.Shortener.Api.UnitTests/Domain/Url/Get/Builder/GetUrlRequestValidatorBuilder.cs
@@ -0,0 +1,8 @@
+﻿using Url.Shortener.Api.Domain.Url.Get;
+
+namespace Url.Shortener.Api.UnitTests.Domain.Url.Get.Builder;
+
+internal static class GetUrlRequestValidatorBuilder
+{
+    public static GetUrlRequestValidator Build() => new();
+}

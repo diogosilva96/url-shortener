25b4d3964b355f17eff4fc5f7c40d5aa7036c5f2
Diogo Silva <diogo_silva_96@hotmail.com>
2023-09-26 18:18:48 +0100

    Add ValidationResultBuilder

---
+++ test/Url.Shortener.Api.Tests.Common/Builder/ValidationResultBuilder.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation.Results;
+
+namespace Url.Shortener.Api.Tests.Common.Builder;
+
+public class ValidationResultBuilder
+{
+    private readonly List<ValidationFailure> _failures;
+
+    public ValidationResultBuilder() => _failures = new();
+
+    public ValidationResult Build() => new(_failures);
+
+    public ValidationResultBuilder WithFailures(IEnumerable<ValidationFailure> failures)
+    {
+        _failures.AddRange(failures);
+
+        return this;
+    }
+}

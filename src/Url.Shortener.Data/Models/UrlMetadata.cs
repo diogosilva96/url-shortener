76814bdded796ac0361b4fff1adb8194af0eced3
Diogo Silva <diogo_silva_96@hotmail.com>
2023-09-27 15:24:41 +0100

    Cleanup

--- src/Url.Shortener.Data/Models/UrlMetadata.cs
+++ src/Url.Shortener.Data/Models/UrlMetadata.cs
@@ -5,8 +5,8 @@ public class UrlMetadata
     public required Guid Id { get; init; }
 
     public required string ShortUrl { get; set; } = string.Empty;
-    
+
     public required string FullUrl { get; set; } = string.Empty;
-    
+
     public required DateTimeOffset CreatedAtUtc { get; set; }
 }

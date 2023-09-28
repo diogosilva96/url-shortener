3b8f3f4752923bbf989a43ce32ab1ca82eb43368
Diogo Silva <diogo_silva_96@hotmail.com>
2023-09-28 19:44:02 +0100

    Remove DbContextHealthCheck & use Microsoft.Extensions.Diagnostics.HealthChecks.EntityFrameworkCore instead

--- src/Url.Shortener.Api/Health/ServiceCollectionExtensions.cs
+++ src/Url.Shortener.Api/Health/ServiceCollectionExtensions.cs
@@ -6,6 +6,6 @@ internal static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddHealthServices(this IServiceCollection serviceCollection) =>
         serviceCollection.AddHealthChecks()
-                         .AddCheck<DbContextHealthCheck<UrlShortenerDbContext>>("UrlShortenerDb", tags: new[] { HealthTags.Live })
+                         .AddDbContextCheck<UrlShortenerDbContext>("UrlShortenerDb", tags: new[] { HealthTags.Live })
                          .Services;
 }

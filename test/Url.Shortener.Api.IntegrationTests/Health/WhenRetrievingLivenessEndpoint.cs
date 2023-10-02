7c5bd591b2ced4c33f52c7374e779dfa9be1bdb2
Diogo Silva <diogo_silva_96@hotmail.com>
2023-10-02 20:16:36 +0100

    Add integration tests for liveness & readiness endpoints

---
+++ test/Url.Shortener.Api.IntegrationTests/Health/WhenRetrievingLivenessEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using Url.Shortener.Api.IntegrationTests.Utils;
+using Xunit;
+
+namespace Url.Shortener.Api.IntegrationTests.Health;
+
+public class WhenRetrievingLivenessEndpoint : IntegrationTestBase
+{
+    private readonly HttpClient _client;
+
+    public WhenRetrievingLivenessEndpoint(IntegrationTestWebApplicationFactory webApplicationFactory) : base(webApplicationFactory) =>
+        _client = webApplicationFactory.CreateClient();
+
+    [Fact]
+    public async Task ThenNoExceptionIsThrown()
+    {
+        var exception = await Record.ExceptionAsync(WhenRetrievingAsync);
+
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public async Task ThenAnOkResponseIsReturned()
+    {
+        var response = await WhenRetrievingAsync();
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    private async Task<HttpResponseMessage> WhenRetrievingAsync() => await _client.GetAsync("health/live");
+}

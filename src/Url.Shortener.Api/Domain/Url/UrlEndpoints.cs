2db5c8bb6441c852d094d05d56fe0a6ed168ac4c
Diogo Silva <diogo_silva_96@hotmail.com>
2023-10-02 20:53:50 +0100

    Refactor UrlEndpoints so that they are integration testable

--- src/Url.Shortener.Api/Domain/Url/UrlEndpoints.cs
+++ src/Url.Shortener.Api/Domain/Url/UrlEndpoints.cs
@@ -14,28 +14,32 @@ public class UrlEndpoints : ICarterModule
         var group = app.MapGroup("api/urls")
                        .WithOpenApi();
 
-        group.MapPost("", async (CreateUrlRequest request, IMediator mediator, CancellationToken cancellationToken) =>
-             {
-                 var domainRequest = new Create.CreateUrlRequest(request.Url);
-                 return TypedResults.Ok(await mediator.Send(domainRequest, cancellationToken));
-             })
+        group.MapPost("", CreateUrl)
              .WithName("CreateUrl")
              .WithDescription("Creates a short url based on the specified request.")
              .Produces<string>()
              .Produces<HttpValidationProblemDetails>(StatusCodes.Status400BadRequest)
              .Produces<ProblemHttpResult>(StatusCodes.Status500InternalServerError);
 
-        group.MapGet("{shortUrl}", async ([Required] string shortUrl, IMediator mediator, CancellationToken cancellationToken) =>
-        {
-            var domainRequest = new GetUrlRequest(shortUrl);
-            var redirectUrl = await mediator.Send(domainRequest, cancellationToken);
-            return TypedResults.Redirect(redirectUrl, permanent: true);
-        })
+        group.MapGet("{shortUrl}", GetUrl)
         .WithName("GetUrl")
         .WithDescription("Redirects the request based on the specified short url.")
         .Produces(StatusCodes.Status308PermanentRedirect)
         .Produces<HttpValidationProblemDetails>(StatusCodes.Status400BadRequest)
         .Produces<NotFound>(StatusCodes.Status404NotFound)
         .Produces<ProblemHttpResult>(StatusCodes.Status500InternalServerError);
+    }
+    
+    public static async Task<IResult> CreateUrl(CreateUrlRequest request, IMediator mediator, CancellationToken cancellationToken = default)
+    {
+        var domainRequest = new Create.CreateUrlRequest(request.Url);
+        return TypedResults.Ok(await mediator.Send(domainRequest, cancellationToken));
+    }
+
+    public static async Task<IResult> GetUrl(string shortUrl, IMediator mediator, CancellationToken cancellationToken = default)
+    {
+        var domainRequest = new GetUrlRequest(shortUrl);
+        var redirectUrl = await mediator.Send(domainRequest, cancellationToken);
+        return TypedResults.Redirect(redirectUrl, permanent: true);
     }
 }

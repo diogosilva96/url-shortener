d257d9f2cd7254bc7fb3521424fa6e83ad817fff
Diogo Silva <diogo_silva_96@hotmail.com>
2023-10-02 20:43:09 +0100

    Add UrlMetadataBuilder

---
+++ test/Url.Shortener.Api.IntegrationTests/Data/Builder/UrlMetadataBuilder.cs
@@ -0,0 +1,15 @@
+﻿using AutoFixture;
+using Url.Shortener.Api.Tests.Common.Builder;
+using Url.Shortener.Data.Models;
+
+namespace Url.Shortener.Api.IntegrationTests.Data.Builder;
+
+internal class UrlMetadataBuilder : TestBuilderBase<UrlMetadata>
+{
+    protected override UrlMetadata CreateDefault() =>
+        Fixture.Build<UrlMetadata>()
+               .With(x => x.ShortUrl, Fixture.Create<Uri>().ToString)
+               .With(x => x.FullUrl, Fixture.Create<string>())
+               .With(x => x.CreatedAtUtc, DateTimeOffset.UtcNow)
+               .Create();
+}

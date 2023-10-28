92bc8cd218db0601a6311678a8435f323f0563a5
Diogo Silva <diogo_silva_96@hotmail.com>
2023-10-28 22:40:54 +0100

    Add MemoryCacheBuilder

---
+++ test/Url.Shortener.Api.UnitTests/Builder/MemoryCacheBuilder.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace Url.Shortener.Api.UnitTests.Builder;
+
+internal class MemoryCacheBuilder
+{
+    private readonly List<Action<IMemoryCache>> _setupActions;
+
+    public MemoryCacheBuilder() => _setupActions = new();
+
+    public IMemoryCache Build()
+    {
+        var memoryCache = new MemoryCache(new MemoryCacheOptions());
+        foreach (var setupAction in _setupActions)
+        {
+            setupAction.Invoke(memoryCache);
+        }
+
+        return memoryCache;
+    }
+
+    public MemoryCacheBuilder Setup(string key, object value)
+    {
+        _setupActions.Add(memoryCache => memoryCache.Set(key, value));
+
+        return this;
+    }
+}

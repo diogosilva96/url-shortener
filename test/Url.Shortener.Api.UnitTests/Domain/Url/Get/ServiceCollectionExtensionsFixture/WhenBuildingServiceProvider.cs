7859923f99742ae190acb4f2e563e44f6e79958f
Diogo Silva <diogo_silva_96@hotmail.com>
2023-10-28 22:32:57 +0100

    Add unit tests for DI registration of 'Url/Get' slice

---
+++ test/Url.Shortener.Api.UnitTests/Domain/Url/Get/ServiceCollectionExtensionsFixture/WhenBuildingServiceProvider.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+using Url.Shortener.Api.Domain.Url.Get;
+using Url.Shortener.Api.UnitTests.Builder;
+using Url.Shortener.Data;
+using Xunit;
+
+namespace Url.Shortener.Api.UnitTests.Domain.Url.Get.ServiceCollectionExtensionsFixture;
+
+public class WhenBuildingServiceProvider
+{
+    private readonly IServiceCollection _serviceCollection;
+
+    public WhenBuildingServiceProvider() => 
+        _serviceCollection = ServiceCollectionBuilder.Build()
+                                                     .AddSingleton(Substitute.For<ApplicationDbContext>())
+                                                     .AddLogging();
+
+    [Fact]
+    public void TheNoExceptionIsThrown()
+    {
+        var exception = Record.Exception(WhenBuilding);
+
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void ThenAMemoryCacheCanBeRetrieved()
+    {
+        var provider = WhenBuilding();
+
+        Assert.NotNull(provider.GetService<IMemoryCache>());
+    }
+
+    [Fact]
+    public void ThenAGetUrlRequestHandlerCanBeRetrieved()
+    {
+        var provider = WhenBuilding();
+
+        Assert.NotNull(provider.GetService<GetUrlRequestHandler>());
+    }
+
+    private IServiceProvider WhenBuilding() => _serviceCollection.AddGetUrlServices()
+                                                                 .BuildServiceProvider();
+}

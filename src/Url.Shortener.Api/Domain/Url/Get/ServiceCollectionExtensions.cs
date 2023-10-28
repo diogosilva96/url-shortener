3a99c38440d9a1a338bcba56fc28bfebf9d5cacc
Diogo Silva <diogo_silva_96@hotmail.com>
2023-10-28 22:32:39 +0100

    Add CacheGetUrlRequestHandler & DI registration for 'Url/Get' slice

---
+++ src/Url.Shortener.Api/Domain/Url/Get/ServiceCollectionExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Url.Shortener.Api.Domain.Url.Get;
+
+internal static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddGetUrlServices(this IServiceCollection serviceCollection) =>
+        serviceCollection.AddMemoryCache()
+                         .AddTransient<GetUrlRequestHandler>();
+}
--- src/Url.Shortener.Api/Domain/Url/ServiceCollectionExtensions.cs
+++ src/Url.Shortener.Api/Domain/Url/ServiceCollectionExtensions.cs
@@ -1,9 +1,12 @@
 ﻿using Url.Shortener.Api.Domain.Url.Create;
+using Url.Shortener.Api.Domain.Url.Get;
 
 namespace Url.Shortener.Api.Domain.Url;
 
 internal static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddUrlServices(this IServiceCollection serviceCollection, 
-        Action<UrlShortenerOptions> configureUrlShortenerOptions) => serviceCollection.AddCreateUrlServices(configureUrlShortenerOptions);
+    public static IServiceCollection AddUrlServices(this IServiceCollection serviceCollection,
+        Action<UrlShortenerOptions> configureUrlShortenerOptions) =>
+        serviceCollection.AddCreateUrlServices(configureUrlShortenerOptions)
+                         .AddGetUrlServices();
 }

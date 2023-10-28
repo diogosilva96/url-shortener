679df7527faff160807b9ce735f25ef31b64db08
Diogo Silva <diogo_silva_96@hotmail.com>
2023-10-28 22:19:44 +0100

    update url endpoint & .http files

--- src/Url.Shortener.Api/Domain/Url/UrlEndpoints.cs
+++ src/Url.Shortener.Api/Domain/Url/UrlEndpoints.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using Carter;
+﻿using Carter;
 using MediatR;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Url.Shortener.Api.Contracts;
@@ -11,10 +10,10 @@ public class UrlEndpoints : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        var group = app.MapGroup("api/urls")
+        var group = app.MapGroup("")
                        .WithOpenApi();
 
-        group.MapPost("", CreateUrlAsync)
+        group.MapPost("api/urls", CreateUrlAsync)
              .WithName("CreateUrl")
              .WithDescription("Creates a short url based on the specified request.")
              .Produces<string>()
@@ -22,14 +21,14 @@ public class UrlEndpoints : ICarterModule
              .Produces<ProblemHttpResult>(StatusCodes.Status500InternalServerError);
 
         group.MapGet("{shortUrl}", GetUrlAsync)
-        .WithName("GetUrl")
-        .WithDescription("Redirects the request based on the specified short url.")
-        .Produces(StatusCodes.Status308PermanentRedirect)
-        .Produces<HttpValidationProblemDetails>(StatusCodes.Status400BadRequest)
-        .Produces<NotFound>(StatusCodes.Status404NotFound)
-        .Produces<ProblemHttpResult>(StatusCodes.Status500InternalServerError);
+             .WithName("GetUrl")
+             .WithDescription("Redirects the request based on the specified short url.")
+             .Produces(StatusCodes.Status308PermanentRedirect)
+             .Produces<HttpValidationProblemDetails>(StatusCodes.Status400BadRequest)
+             .Produces<NotFound>(StatusCodes.Status404NotFound)
+             .Produces<ProblemHttpResult>(StatusCodes.Status500InternalServerError);
     }
-    
+
     public static async Task<IResult> CreateUrlAsync(CreateUrlRequest request, IMediator mediator, CancellationToken cancellationToken = default)
     {
         var domainRequest = new Create.CreateUrlRequest(request.Url);
@@ -41,6 +40,6 @@ public class UrlEndpoints : ICarterModule
     {
         var domainRequest = new GetUrlRequest(shortUrl);
         var redirectUrl = await mediator.Send(domainRequest, cancellationToken);
-        return TypedResults.Redirect(redirectUrl, permanent: true, preserveMethod: true);
+        return TypedResults.Redirect(redirectUrl, true, true);
     }
 }

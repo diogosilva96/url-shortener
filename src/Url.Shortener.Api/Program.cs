bdd23549b575a5c2eb38e0c417575d06b30dea2a
Diogo Silva <diogo_silva_96@hotmail.com>
2023-10-29 10:07:39 +0000

    Add ConfigurationConfigurator & prefixed env variables

--- src/Url.Shortener.Api/Program.cs
+++ src/Url.Shortener.Api/Program.cs
@@ -2,14 +2,8 @@ using Url.Shortener.Api;
 
 var builder = WebApplication.CreateBuilder(args);
 
-if (builder.Environment.IsDevelopment())
-{
-    // allow user secrets in development - Note: development is treated as a local environment here as this is just a sample application.
-    builder.Configuration.AddUserSecrets(typeof(Program).Assembly);
-}
-
-builder.Host.ConfigureSerilogAsOnlyLoggingProvider(builder.Configuration);
-
+builder.Configuration.Configure(builder.Environment);
+builder.ConfigureSerilogAsOnlyLoggingProvider(builder.Configuration);
 builder.Services.RegisterServices(builder.Configuration, builder.Environment);
 
 var app = builder.Build();

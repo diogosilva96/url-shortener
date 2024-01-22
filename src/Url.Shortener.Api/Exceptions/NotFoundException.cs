e88f8263374b84d5892cc62f93696736e09d2773
Diogo Silva <diogo_silva_96@hotmail.com>
2024-01-22 19:09:17 +0000

    Fix constructor

--- src/Url.Shortener.Api/Exceptions/NotFoundException.cs
+++ src/Url.Shortener.Api/Exceptions/NotFoundException.cs
@@ -3,7 +3,5 @@
 internal class NotFoundException : Exception
 {
     public NotFoundException(string? message = default, Exception? innerException = default) : base(message, innerException)
-    {
-        
-    }
+    { }
 }

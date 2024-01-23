0b53749c356574560c91f1458e9adcd4e792f125
Diogo Silva <diogo_silva_96@hotmail.com>
2024-01-23 18:49:47 +0000

    Fix UrlMetadata

--- src/Url.Shortener.Api/Contracts/UrlMetadata.cs
+++ src/Url.Shortener.Api/Contracts/UrlMetadata.cs
@@ -2,8 +2,6 @@
 
 public class UrlMetadata
 {
-    public required Guid Id { get; init; }
-
     public required string ShortUrl { get; init; } = string.Empty;
 
     public required string FullUrl { get; init; } = string.Empty;

a71cdf02b6249ef13c6a7fc77e03c4af8093f38b
Diogo Silva <diogo_silva_96@hotmail.com>
2024-01-23 18:45:44 +0000

    Add UrlMetadata contract

---
+++ src/Url.Shortener.Api/Contracts/UrlMetadata.cs
@@ -0,0 +1,12 @@
+﻿namespace Url.Shortener.Api.Contracts;
+
+public class UrlMetadata
+{
+    public required Guid Id { get; init; }
+
+    public required string ShortUrl { get; init; } = string.Empty;
+
+    public required string FullUrl { get; init; } = string.Empty;
+
+    public required DateTimeOffset CreatedAtUtc { get; init; }
+}

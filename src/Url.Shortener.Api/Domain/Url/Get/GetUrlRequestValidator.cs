5a6024c326edf907da265d0e414bda7f55daa3a5
Diogo Silva <diogo_silva_96@hotmail.com>
2024-01-23 19:25:44 +0000

    Add EnsureValidShortUrl RuleBuilder extension method

--- src/Url.Shortener.Api/Domain/Url/Get/GetUrlRequestValidator.cs
+++ src/Url.Shortener.Api/Domain/Url/Get/GetUrlRequestValidator.cs
@@ -6,12 +6,6 @@ internal class GetUrlRequestValidator : AbstractValidator<GetUrlRequest>
 {
     public GetUrlRequestValidator()
     {
-        // TODO: make an extension method for this so that it can be reused with the CreateUrl endpoint
-        RuleFor(x => x.ShortUrl).MinimumLength(5)
-                                .MaximumLength(50)
-                                .Must(x => Uri.IsWellFormedUriString(x, UriKind.Relative))
-                                .WithMessage("The '{PropertyName}' must be a valid relative uri.")
-                                .Must(x => !x.Contains('/'))
-                                .WithMessage("The '{PropertyName}' must not contain a path separator character ('/').");
+        RuleFor(x => x.ShortUrl).EnsureValidShortUrl();
     }
 }
---
+++ src/Url.Shortener.Api/Domain/Url/RuleBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Url.Shortener.Api.Domain.Url;
+
+internal static class RuleBuilderExtensions
+{
+    public static IRuleBuilderOptions<T, string> EnsureValidShortUrl<T>(this IRuleBuilder<T, string> ruleBuilder) => 
+        ruleBuilder.MinimumLength(5)
+                   .MaximumLength(50)
+                   .Must(x => Uri.IsWellFormedUriString(x, UriKind.Relative))
+                   .WithMessage("The '{PropertyName}' must be a valid relative uri.")
+                   .Must(x => !x.Contains('/'))
+                   .WithMessage("The '{PropertyName}' must not contain a path separator character ('/').");
+}

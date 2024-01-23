53910afd0f512b3b9052673596f48e9175f5a464
Diogo Silva <diogo_silva_96@hotmail.com>
2024-01-23 20:01:45 +0000

    Add unit tests for GetUrlRequestHandler

--- src/Url.Shortener.Api/Domain/Url/Get/GetUrlRequestHandler.cs
+++ src/Url.Shortener.Api/Domain/Url/Get/GetUrlRequestHandler.cs
@@ -8,14 +8,17 @@ namespace Url.Shortener.Api.Domain.Url.Get;
 internal class GetUrlRequestHandler : IRequestHandler<GetUrlRequest, UrlMetadata>
 {
     private readonly ApplicationDbContext _dbContext;
-    
-    public GetUrlRequestHandler(ApplicationDbContext dbContext)
+    private readonly ILogger<GetUrlRequestHandler> _logger;
+
+    public GetUrlRequestHandler(ApplicationDbContext dbContext, ILogger<GetUrlRequestHandler> logger)
     {
         _dbContext = dbContext;
+        _logger = logger;
     }
     public async Task<UrlMetadata> Handle(GetUrlRequest request, CancellationToken cancellationToken)
     {
-        // TODO: make url metadata fetch reusable
+        // TODO: make url metadata fetch reusable 
+        // TODO: add caching
         var metadata = await _dbContext.UrlMetadata
                                        .Where(x => x.ShortUrl == request.ShortUrl)
                                        .Select(x => new UrlMetadata() 
@@ -26,8 +29,10 @@ internal class GetUrlRequestHandler : IRequestHandler<GetUrlRequest, UrlMetadata
                                        })
                                        .SingleOrDefaultAsync(cancellationToken);
 
+        // ReSharper disable once InvertIf
         if (metadata is null)
         {
+            _logger.LogWarning("Could not find url metadata for short url '{ShortUrl}'", request.ShortUrl);
             throw GetUrlExceptions.UrlNotFound();
         }
 
---
+++ test/Url.Shortener.Api.UnitTests/Domain/Url/Get/GetUrlRequestHandlerFixture/WhenHandlingRequestAndUrlMetadataCannotBeFound.cs
@@ -0,0 +1,62 @@
+﻿using Url.Shortener.Api.Domain.Url.Get;
+using Url.Shortener.Api.Exceptions;
+using Url.Shortener.Api.UnitTests.Data.Builder;
+using Url.Shortener.Api.UnitTests.Domain.Url.Get.Builder;
+using Xunit;
+
+namespace Url.Shortener.Api.UnitTests.Domain.Url.Get.GetUrlRequestHandlerFixture;
+
+public class WhenHandlingRequestAndUrlMetadataCannotBeFound
+{
+    private readonly GetUrlRequestHandler _handler;
+    private readonly GetUrlRequest _request;
+
+    public WhenHandlingRequestAndUrlMetadataCannotBeFound()
+    {
+        _request = new("123");
+        
+        var metadata = new[]
+        {
+            new UrlMetadataBuilder().With(x => x.ShortUrl = "1")
+                                    .Build(),
+            new UrlMetadataBuilder().With(x => x.ShortUrl = "2")
+                                    .Build()
+        };
+        
+
+        var dbContext = new UrlShortenerDbContextBuilder().With(metadata)
+                                                           .Build();
+
+        _handler = new GetUrlRequestHandlerBuilder().With(dbContext)
+                                                    .Build();
+    }
+
+    [Fact]
+    public async Task ThenAnExceptionIsThrown()
+    {
+        var exception = await Record.ExceptionAsync(WhenHandlingAsync);
+
+        Assert.NotNull(exception);
+    }
+
+    [Fact]
+    public async Task ThenANotFoundExceptionIsThrown()
+    {
+        var exception = await Record.ExceptionAsync(WhenHandlingAsync);
+
+        Assert.IsType<NotFoundException>(exception);
+    }
+
+    [Fact]
+    public async Task ThenTheExpectedNotFoundExceptionIsThrown()
+    {
+        var exception = await Record.ExceptionAsync(WhenHandlingAsync);
+
+        var notFoundException = (exception as NotFoundException)!;
+        var expectedException = GetUrlExceptions.UrlNotFound();
+
+        Assert.Equal(expectedException.Message, notFoundException.Message);
+    }
+
+    private async Task<Contracts.UrlMetadata> WhenHandlingAsync() => await _handler.Handle(_request, CancellationToken.None);
+}

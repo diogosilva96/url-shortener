2812e5ff0c9742a5a7372e70bac34781dcd6d2f5
Diogo Silva <diogo_silva_96@hotmail.com>
2024-03-02 14:16:56 +0000

    Add implementation for ListUrlRequestHandler

--- src/Url.Shortener.Api/Domain/Url/List/ListUrlRequestHandler.cs
+++ src/Url.Shortener.Api/Domain/Url/List/ListUrlRequestHandler.cs
@@ -1,9 +1,43 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Url.Shortener.Api.Contracts;
+using Url.Shortener.Data;
 
 namespace Url.Shortener.Api.Domain.Url.List;
 
 public class ListUrlRequestHandler : IRequestHandler<ListUrlRequest, PagedResult<UrlMetadata>>
 {
-    public Task<PagedResult<UrlMetadata>> Handle(ListUrlRequest request, CancellationToken cancellationToken) => throw new NotImplementedException();
+    private readonly ApplicationDbContext _dbContext;
+
+    public ListUrlRequestHandler(ApplicationDbContext dbContext) => _dbContext = dbContext;
+
+    public async Task<PagedResult<UrlMetadata>> Handle(ListUrlRequest request, CancellationToken cancellationToken)
+    {
+        var queryable = _dbContext.UrlMetadata;
+        var totalCount = await queryable.CountAsync(cancellationToken);
+        if (totalCount == 0)
+        {
+            return new()
+            {
+                Data = Enumerable.Empty<UrlMetadata>(),
+                Page = request.Page,
+                PageSize = request.PageSize,
+                PageCount = totalCount
+            };
+        }
+
+        var itemsToSkip = request.PageSize - 1 * request.Page;
+        var items = await queryable.Skip(itemsToSkip)
+                                   .Take(request.PageSize)
+                                   .MapToUrlMetadataContract()
+                                   .ToArrayAsync(cancellationToken);
+
+        return new()
+        {
+            Data = items,
+            Page = request.Page,
+            PageSize = request.PageSize,
+            PageCount = totalCount
+        };
+    }
 }
